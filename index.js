f3c1fa647f861c12a1a662e0a01c53883c446fa8
mousezero <rm@mousezero.com>
2017-02-04 21:20:28 -0800

    refactor

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const date = new Date();
 const fs = require('fs');
 const Transform = require('stream').Transform;
 
-fs.open('time.csv', 'wx', ()=>{});
+function start(){
+  createCSVIfNeeded();
+
+  if( noComma(desc + project) ){
+    addCurrentTime();
+    console.log('adding time!!!')
+  } else {
+    console.log('Error :::: please don\'t use commas (time not recorded)');
+  }
+}
+
+function noComma(s){
+  return s.indexOf(',') === -1
+}
+
+function createCSVIfNeeded(){
+  fs.open('time.csv', 'wx', ()=>{});
+}
 
 class TimeElapsed extends Transform{
   constructor(options){
@@ -15,6 +32,7 @@ TimeElapsed.prototype._transform = function (chunk, enc, cb){
   const allLines = chunk.toString().split('\n').map(x => {
     const line = x.split(',');
     if(line.length < 3) return '';
+    // 0 = punch time, 1 = duration, 2 = description, 3 = type
     if(!line[1] && line[2] !== 'out'){
       line[1] = (toExcelTime(date) - line[0]);
     }
@@ -49,9 +67,4 @@ function addCurrentTime(){
   write.on('error', x => console.log(x));
 }
 
-if(desc.indexOf(',') > -1 || project.indexOf(',') > -1){
-  console.log('Error :::: please don\'t use commas (time not recorded)');
-}else{
-  addCurrentTime();
-  console.log('adding time!!!')
-}
+start();
